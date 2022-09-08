03c6de88b61a1e1d503010f60f0d9b86e6179106
unknown <nwosu4686@gmail.com>
2022-09-08 15:38:24 +0100

    result generator

--- color-picker.js
+++ color-picker.js
@@ -4,6 +4,7 @@ let boxThree = document.querySelector("#boxThree");
 let boxFour = document.querySelector("#boxFour");
 let boxFive = document.querySelector("#boxFive");
 let boxSix = document.querySelector("#boxSix");
+let result = document.querySelector("h1");
 
 // This function generates random numbers from min to max.
 const getRndInteger = (min, max) => {
@@ -19,6 +20,13 @@ const colorGenetator = () => {
     return `rgb(${r}, ${g}, ${b})`;
 }
 
+// This function generates result 
+const resultGenerator = () => {
+    const boxArray = [boxOneValue, boxTwoValue, boxThreeValue, boxFourValue, boxFiveValue, boxSixValue];
+
+    return boxArray[getRndInteger(0, 5)];
+}
+
 let boxOneValue = colorGenetator();
 let boxTwoValue = colorGenetator();
 let boxThreeValue = colorGenetator();
@@ -26,10 +34,21 @@ let boxFourValue = colorGenetator();
 let boxFiveValue = colorGenetator();
 let boxSixValue = colorGenetator();
 
-// This assigns random colors to be displayed in each box.
+// These statements assigns random colors to be displayed in each box.
 boxOne.style.backgroundColor = boxOneValue;
 boxTwo.style.backgroundColor = boxTwoValue;
 boxThree.style.backgroundColor = boxThreeValue;
 boxFour.style.backgroundColor = boxFourValue;
 boxFive.style.backgroundColor = boxFiveValue;
 boxSix.style.backgroundColor = boxSixValue;
+
+// This statement displays the result generated to the user.
+result.innerHTML = resultGenerator();
+
+console.log(`box1: ${boxOneValue}`);
+console.log(`box2: ${boxTwoValue}`);
+console.log(`box3: ${boxThreeValue}`);
+console.log(`box4: ${boxFourValue}`);
+console.log(`box5: ${boxFiveValue}`);
+console.log(`box6: ${boxSixValue}`);
+
