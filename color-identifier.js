fb12cf9cdafea21ecbe31c94745d50f0894a1e41
User-XYZ <nwosu4686@gmail.com>
2022-12-04 21:34:43 +0100

    modified code apperance

--- color-identifier.js
+++ color-identifier.js
@@ -11,6 +11,8 @@ let boxSix = document.querySelector("#boxSix");
 let mode = document.querySelector(".darkMode");
 let modeButton = document.querySelector("i");
 
+const boxValueArray = new Array(6);
+const boxElementArray = [boxOne, boxTwo, boxThree, boxFour, boxFive, boxSix];
 
 // This function changes the mode of the application between dark and light.
 const modeChange = () => {
@@ -46,38 +48,17 @@ const colorGenetator = () => {
 }
 
 // This function generates result 
-const resultGenerator = () => {
-    const boxArray = [boxOneValue, boxTwoValue, boxThreeValue, boxFourValue, boxFiveValue, boxSixValue];
-
-    return boxArray[getRndInteger(0, 5)];
-}
+const resultGenerator = () => boxValueArray[getRndInteger(0, 5)];
 
 // This function checks if the option the user selects is the right option.
 const resultVerifier = (value, element) => {
     if (value === result.innerHTML) {
-        /* These statements gives all boxes the same color-value, color and make them visible. */
-
-        // 55-60 stops a box from being clicked after the right box has been clicked.
-        boxOneValue = value; 
-        boxTwoValue = value;
-        boxThreeValue = value;
-        boxFourValue = value;
-        boxFiveValue = value;
-        boxSixValue = value;
-
-        boxOne.style.backgroundColor = value;
-        boxTwo.style.backgroundColor = value;
-        boxThree.style.backgroundColor = value;
-        boxFour.style.backgroundColor = value;
-        boxFive.style.backgroundColor = value;
-        boxSix.style.backgroundColor = value;
-
-        boxOne.style.visibility = "visible";
-        boxTwo.style.visibility = "visible";
-        boxThree.style.visibility = "visible";
-        boxFour.style.visibility = "visible";
-        boxFive.style.visibility = "visible";
-        boxSix.style.visibility = "visible";
+        /* This block gives all boxes the same color-value, color and make them visible. */
+        for (let i = 0; i < boxValueArray.length; i++) {
+            boxValueArray[i] = value;
+            boxElementArray[i].style.backgroundColor = value;
+            boxElementArray[i].style.visibility = "visible";
+        }
 
         text.innerHTML = "Correct!";
         text.style.color = "rgb(0, 255, 0)";
@@ -91,29 +72,12 @@ const resultVerifier = (value, element) => {
 
 // This function generates the game.
 const gameGenerator = () => {
-    // These statements assigns random valid rgb values.
-    boxOneValue = colorGenetator();
-    boxTwoValue = colorGenetator();
-    boxThreeValue = colorGenetator();
-    boxFourValue = colorGenetator();
-    boxFiveValue = colorGenetator();
-    boxSixValue = colorGenetator();
-
-    // These statements assigns random colors to be displayed in each box.
-    boxOne.style.backgroundColor = boxOneValue;
-    boxTwo.style.backgroundColor = boxTwoValue;
-    boxThree.style.backgroundColor = boxThreeValue;
-    boxFour.style.backgroundColor = boxFourValue;
-    boxFive.style.backgroundColor = boxFiveValue;
-    boxSix.style.backgroundColor = boxSixValue;
-
-    // These statements is to make all the boxes visible (NOTE: this fixed the bug in the reset button)
-    boxOne.style.visibility = "visible";
-    boxTwo.style.visibility = "visible";
-    boxThree.style.visibility = "visible";
-    boxFour.style.visibility = "visible";
-    boxFive.style.visibility = "visible";
-    boxSix.style.visibility = "visible";
+    // This block assigns random colors to be displayed in each box.
+    for(let i = 0; i < boxValueArray.length; i++) {
+        boxValueArray[i] = colorGenetator();
+        boxElementArray[i].style.backgroundColor = boxValueArray[i];
+        boxElementArray[i].style.visibility = "visible";
+    }
 
     // This statement displays the result generated to the user.
     result.innerHTML = resultGenerator();
@@ -131,11 +95,11 @@ gameGenerator(); // Loads the game when application is refreshed.
 
 /*        THIS SECTION HANDLES EVENTS        */
 
-reset.addEventListener('click', () => {gameGenerator();})
-boxOne.addEventListener('click', () => {resultVerifier(boxOneValue, boxOne);})
-boxTwo.addEventListener('click', () => {resultVerifier(boxTwoValue, boxTwo);})
-boxThree.addEventListener('click', () => {resultVerifier(boxThreeValue, boxThree);})
-boxFour.addEventListener('click', () => {resultVerifier(boxFourValue, boxFour);})
-boxFive.addEventListener('click', () => {resultVerifier(boxFiveValue, boxFive);})
-boxSix.addEventListener('click', () => {resultVerifier(boxSixValue, boxSix);})
-modeButton.addEventListener('click', () => {modeChange();})
+reset.addEventListener('click', () => gameGenerator())
+boxOne.addEventListener('click', () => resultVerifier(boxValueArray[0], boxOne))
+boxTwo.addEventListener('click', () => resultVerifier(boxValueArray[1], boxTwo))
+boxThree.addEventListener('click', () => resultVerifier(boxValueArray[2], boxThree))
+boxFour.addEventListener('click', () => resultVerifier(boxValueArray[3], boxFour))
+boxFive.addEventListener('click', () => resultVerifier(boxValueArray[4], boxFive))
+boxSix.addEventListener('click', () => resultVerifier(boxValueArray[5], boxSix))
+modeButton.addEventListener('click', () => modeChange())
